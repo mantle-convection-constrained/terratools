3a43f494dc3e375dcde30b4e63549ab640aac372
eejwa <eejwa@leeds.ac.uk>
2022-09-08 16:57:30 +0100

    update tests for lookup_table.py
    updated the class to use assertAlmostEqual and debugged the testing itself.

--- tests/test_lookup_table.py
+++ tests/test_lookup_table.py
@@ -3,40 +3,45 @@ import unittest
 import numpy as np
 from terratools.lookup_tables import SeismicLookupTable
 
-lookup_tab_path = './data/test_lookup_table.txt'
-
-
+# lookup_tab_path = './data/test_lookup_table.txt'
 
 class TestLookup(unittest.TestCase):
 
     def setUp(self):
-        self.tab = SeismicLookupTable(lookup_tab_path)
+        self.lookup_tab_path = './data/test_lookup_table.txt'
+        self.tab = SeismicLookupTable(self.lookup_tab_path)
 
     def test_read_file(self):
         
         # test table has been read in correctly by comparing the
         # size of the array read in.
-        self.self.assertEqual(self.tab.table.shape, (121, 10), 
+        self.assertEqual(self.tab.table.shape, (121, 10), 
                               'file not read in correctly')
 
-        self.self.assertEqual(self.tab.table[0,0], -50.0, 
+        self.assertEqual(self.tab.table[0,0], -50.0, 
                               'file not read in correctly')
 
     def test_interpolate_point(self):
 
         p_test=15
         t_test=15
-        expected_vp = p_test**2 + t_test**2
 
-        self.assertEqual(self.tab.interp_points(p_test, t_test), expected_vp, 
-                         'interpolation for single point failed')
+        self.assertAlmostEqual(self.tab.interp_points([p_test, t_test], 'Vp'), np.array([15]), 
+                               msg='interpolation for single point failed')
 
     def test_interpolate_grid(self):
 
-        p_test = np.linspace(-20,20,5)
-        t_test = np.linspace(-20,20,5)
+        p_test = [4,5,6]
+        t_test = 10
+        outgrid = self.tab.interp_grid(p_test, t_test, 'Vp')
 
-        expected_vp = np.power(p_test, 2) + np.power(t_test, 2)
+        self.assertAlmostEqual(outgrid[0], 4,
+                               msg='interpolation for grid of points failed')                        
+        self.assertAlmostEqual(outgrid[1], 5,
+                               msg='interpolation for grid of points failed')
+        self.assertAlmostEqual(outgrid[2], 6,
+                               msg='interpolation for grid of points failed')
 
-        self.assertEqual(self.tab.interp_points(p_test, t_test), expected_vp,
-                         'interpolation for grid of points failed')
+
+if __name__ == '__main__':
+    unittest.main()
