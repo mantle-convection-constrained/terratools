80f0d4eddb3372e72429805796026daef06cb4fd
Bob Myhill <myhill.bob@gmail.com>
2022-10-12 16:49:52 +0100

    prebuild examples for RTD

--- docs/copy_ref_examples.py
+++ docs/copy_ref_examples.py
@@ -2,9 +2,12 @@
 
 import os
 import shutil
+import mkdocs_gen_files
+
+nav = mkdocs_gen_files.Nav()
 
 # List of the examples to copy
-examples = ["example_attenuation.py"]
+examples = ["example_attenuation"]
 
 
 # The rest of this script does all the copying
@@ -17,6 +20,16 @@ os.makedirs(f"{path}")
 
 for ex in examples:
     shutil.copyfile(
-        f"examples/{ex}",
-        f"docs/reference_examples/{ex}",
+        f"examples/{ex}.py",
+        f"docs/reference_examples/{ex}.py",
     )
+
+    parts = ex
+    doc_path = f"{ex}.py"
+    nav[parts] = doc_path
+    full_doc_path = f"reference_examples/{ex}.py"
+    path = f"{ex}.py"
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
+
+with mkdocs_gen_files.open("reference_examples/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
