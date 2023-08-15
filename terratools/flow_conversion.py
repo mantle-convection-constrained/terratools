ea24ced0d7e3662049ef46b6dceff648b4992764
James Wookey <j.wookey@bristol.ac.uk>
2023-08-15 15:52:55 +0100

    Changes to flowfield calculations / descriptions.
    This commit updates the calculations of a geographical decription of the
    flowfield to be (a) more intuitively defined, and (b) with an interface
    more consistent with the other parts of the code. Specifically:
    
    * The geographical reference frame for the flow field is updated to
      east-north-up.
    * The model field is updated from u_geog to u_enu to make it more
      descriptive.
    * The interface is updated to (lon,lat) pairs rather than (lat,lon).
    
    It also adds an example of plotting the flowfield on a map.

--- terratools/flow_conversion.py
+++ terratools/flow_conversion.py
@@ -8,29 +8,21 @@ import numpy as np
 import terratools.geographic as gt
 
 
-def get_rotmat_to_geographical(lat, lon):
+def get_rotmat_to_geographical(lon, lat):
     """
-    Return the rotation matrix at a latitude/longitude
+    Return the rotation matrix at a longitude/latitude
     to convert the cartesian flow vector into
     geographical coordinates.
 
-    :param lat: latitude
-    :type lat: float
     :param lon: longitude
     :type lon: float
+    :param lat: latitude
+    :type lat: float
     :return trans: transformation matrix
     :rtype trans: 2D numpy array
     """
 
-    # check latitude is an integer or a float
-    if isinstance(lat, np.ndarray):
-        if lat.dtype.kind != "f" and lat.dtype.kind == "i":
-            raise AssertionError("latitude needs to be integer or float")
-    elif np.isscalar(lat):
-        if not isinstance(lat, (int, float, np.floating, np.integer)):
-            raise AssertionError("latitude needs to be integer or float")
-    else:
-        raise AssertionError("latitude type not acceptable")
+    # check longitude and latitude are integers or floats
 
     if isinstance(lat, np.ndarray):
         if lon.dtype.kind != "f" and lon.dtype.kind == "i":
@@ -41,6 +33,16 @@ def get_rotmat_to_geographical(lat, lon):
     else:
         raise AssertionError("longitude type not acceptable")
 
+    if isinstance(lat, np.ndarray):
+        if lat.dtype.kind != "f" and lat.dtype.kind == "i":
+            raise AssertionError("latitude needs to be integer or float")
+    elif np.isscalar(lat):
+        if not isinstance(lat, (int, float, np.floating, np.integer)):
+            raise AssertionError("latitude needs to be integer or float")
+    else:
+        raise AssertionError("latitude type not acceptable")
+
+
     # Unit vectors on global system
     x_hat = np.array([1.0, 0.0, 0.0])
     y_hat = np.array([0.0, 1.0, 0.0])
@@ -49,44 +51,44 @@ def get_rotmat_to_geographical(lat, lon):
     # Find unit vector pointing to location in cart space
     (x0, y0, z0) = gt.geog2cart(lon, lat, 1.0)
 
-    p_hat = np.array([x0, y0, z0])
-    p_hat = p_hat / np.sqrt(np.sum(np.power(p_hat, 2.0)))
+    u_hat = np.array([x0, y0, z0])
+    u_hat = u_hat / np.sqrt(np.sum(np.power(u_hat, 2.0)))
 
     # Local West and East unit vector(s)
-    e_hat = np.cross(z_hat, p_hat)
+    e_hat = np.cross(z_hat, u_hat)
     e_hat = e_hat / np.sqrt(np.sum(np.power(e_hat, 2.0)))
-    w_hat = np.cross(p_hat, z_hat)
+    w_hat = np.cross(u_hat, z_hat)
     w_hat = w_hat / np.sqrt(np.sum(np.power(w_hat, 2.0)))
 
     # Local North unit vector
-    n_hat = np.cross(p_hat, e_hat)
+    n_hat = np.cross(u_hat, e_hat)
     # No need to normalise, p and e are already orthoganol.
 
     # http://www.kwon3d.com/theory/transform/transform.html
 
     trans = np.array(
         [
+            [np.dot(x_hat, e_hat), np.dot(y_hat, e_hat), np.dot(z_hat, e_hat)],
             [np.dot(x_hat, n_hat), np.dot(y_hat, n_hat), np.dot(z_hat, n_hat)],
-            [np.dot(x_hat, w_hat), np.dot(y_hat, w_hat), np.dot(z_hat, w_hat)],
-            [np.dot(x_hat, p_hat), np.dot(y_hat, p_hat), np.dot(z_hat, p_hat)],
+            [np.dot(x_hat, u_hat), np.dot(y_hat, u_hat), np.dot(z_hat, u_hat)],
         ]
     )
 
     return trans
 
 
-def rotate_vector(vec, lat, lon):
+def rotate_vector(vec, lon, lat):
     """
     Convert the cartesian flow vector into
     geographical vector.
 
     :param vec: cartesian flow vector in [vx,vy,vz]
     :type vec: 1D numpy array of floats
-    :param lat: latitude
-    :type lat: float
     :param lon: longitude
     :type lon: float
-    :return vec: geographical flow vector in [lat, lon, rad]
+    :param lat: latitude
+    :type lat: float
+    :return vec: geographical flow vector in [east, north, up] ('up' is radial +ve outwards)
     :rtype vec: 1D numpy array of floats
     """
 
@@ -100,7 +102,7 @@ def rotate_vector(vec, lat, lon):
         raise AssertionError("flow vector needs to be a numpy array.")
 
     # get transformation matrix
-    trans = get_rotmat_to_geographical(lat, lon)
+    trans = get_rotmat_to_geographical(lon, lat)
     # apply transform to cartesian vector
     vec = np.matmul(vec, np.transpose(trans))
     return vec
