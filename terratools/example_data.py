421ec3bb1297125354c7617b64c150bb04bdb355
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2023-09-07 13:34:56 +0100

    Run indent with black installed

--- terratools/example_data.py
+++ terratools/example_data.py
@@ -1,5 +1,5 @@
 """
-This submodule provides access to example data for TerraTools. Data is 
+This submodule provides access to example data for TerraTools. Data is
 downloaded on first use and stored in a local cache. This is managed
 using pooch with file names returned by each of the public functions in
 the submodule.
@@ -13,9 +13,11 @@ you could run:
 """
 import pooch
 
-_EXAMPLE_DATA = pooch.create(path=pooch.os_cache("terratools"), 
-                             base_url="doi:10.6084/m9.figshare.24100362.v1",
-                             registry=None)
+_EXAMPLE_DATA = pooch.create(
+    path=pooch.os_cache("terratools"),
+    base_url="doi:10.6084/m9.figshare.24100362.v1",
+    registry=None,
+)
 
 _EXAMPLE_DATA.load_registry_from_doi()
 
@@ -24,7 +26,7 @@ def example_bas_table():
     """
     Return the full filename for the basalt example file
     """
-    filename = _EXAMPLE_DATA.fetch('example_bas_table.dat')
+    filename = _EXAMPLE_DATA.fetch("example_bas_table.dat")
     return filename
 
 
@@ -32,14 +34,13 @@ def example_hzb_table():
     """
     Return the full filename for the harzbergite example file
     """
-    filename = _EXAMPLE_DATA.fetch('example_hzb_table.dat')
-    return filename 
+    filename = _EXAMPLE_DATA.fetch("example_hzb_table.dat")
+    return filename
 
 
 def example_lhz_table():
     """
     Return the full filename for the lherzolite example file
     """
-    filename = _EXAMPLE_DATA.fetch('example_lhz_table.dat')
+    filename = _EXAMPLE_DATA.fetch("example_lhz_table.dat")
     return filename
-
