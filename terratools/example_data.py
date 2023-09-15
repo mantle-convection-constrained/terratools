552f7358974ca067ec92063453379ad787ba7b03
Bob Myhill <myhill.bob@gmail.com>
2023-09-15 11:51:36 +0100

    add example blurbs

--- terratools/example_data.py
+++ terratools/example_data.py
@@ -9,7 +9,8 @@ you could run:
 
     import terratools.example_data
     filename = terratools.example_data.example_bas_table()
-    # filename is then the full path to the example data file
+
+The filename is then the full path to the example data file.
 """
 import pooch
 
@@ -25,6 +26,21 @@ _EXAMPLE_DATA.load_registry_from_doi()
 def example_bas_table():
     """
     Return the full filename for the basalt example file
+
+    This file was created using PerpleX with the Stixrude
+    and Lithgow-Bertelloni (2021) dataset, by adapting the
+    script in examples/example_perplex.py
+
+    The P-T range is 0-140 GPa (2 GPa intervals) and
+    300-4300 K (200 K intervals).
+
+    The bulk composition (molar amounts) is:
+    SiO2: 52.298,
+    MgO: 15.812,
+    FeO: 7.121,
+    CaO: 13.027,
+    Al2O3: 9.489,
+    Na2O: 2.244.
     """
     filename = _EXAMPLE_DATA.fetch("example_bas_table.dat")
     return filename
@@ -32,7 +48,22 @@ def example_bas_table():
 
 def example_hzb_table():
     """
-    Return the full filename for the harzbergite example file
+    Return the full filename for the harzburgite example file
+
+    This file was created using PerpleX with the Stixrude
+    and Lithgow-Bertelloni (2021) dataset, by adapting the
+    script in examples/example_perplex.py
+
+    The P-T range is 0-140 GPa (2 GPa intervals) and
+    300-4300 K (200 K intervals).
+
+    The bulk composition (molar amounts) is:
+    SiO2: 36.184,
+    MgO: 56.559,
+    FeO: 5.954,
+    CaO: 0.889,
+    Al2O3: 0.492,
+    Na2O: 0.001.
     """
     filename = _EXAMPLE_DATA.fetch("example_hzb_table.dat")
     return filename
@@ -41,6 +72,21 @@ def example_hzb_table():
 def example_lhz_table():
     """
     Return the full filename for the lherzolite example file
+
+    This file was created using PerpleX with the Stixrude
+    and Lithgow-Bertelloni (2021) dataset, by adapting the
+    script in examples/example_perplex.py
+
+    The P-T range is 0-140 GPa (2 GPa intervals) and
+    300-4300 K (200 K intervals).
+
+    The bulk composition (molar amounts) is:
+    SiO2: 38.819,
+    MgO: 49.894,
+    FeO: 6.145,
+    CaO: 2.874,
+    Al2O3: 1.963,
+    Na2O: 0.367.
     """
     filename = _EXAMPLE_DATA.fetch("example_lhz_table.dat")
     return filename
