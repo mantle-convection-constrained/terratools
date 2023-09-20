de61ac09db7338247af8cd5cc548cc79f8b5b9ef
eejwa <eejwa@leeds.ac.uk>
2023-09-20 14:37:58 +0100

    fix indent

--- examples/example_plot_map.py
+++ examples/example_plot_map.py
@@ -2,13 +2,13 @@
 """
 # example_plot_map
 
-In this example, we show how to read in terra output in NetCDF format 
+In this example, we show how to read in terra output in NetCDF format
 and plot the temperature and bulk composition field at a radius of interest.
 """
 
 # %% [markdown]
 """
-Let's import all the necessary python objects. 
+Let's import all the necessary python objects.
 """
 
 # %%
@@ -16,17 +16,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 from terratools import terra_model as tm
-import cartopy 
-import cartopy.crs as ccrs 
+import cartopy
+import cartopy.crs as ccrs
 
 # %% [markdown]
 """
-Now lets read in the terra output netcdf files and print some summary information. 
+Now lets read in the terra output netcdf files and print some summary information.
 """
 
 # %%
-# Read in the netcdf files. 
-m = tm.read_netcdf(glob.glob('/Users/earjwara/work/terra_models/muller_bb_visc1_m_bb_044_tvisc1_prim_NC_037/NC_037/*'))
+# Read in the netcdf files.
+m = tm.read_netcdf(
+    glob.glob(
+        "/Users/earjwara/work/terra_models/muller_bb_visc1_m_bb_044_tvisc1_prim_NC_037/NC_037/*"
+    )
+)
 print(m)
 
 # %% [markdown]
@@ -37,14 +41,14 @@ Plotting depth slices in terratools is very easy. First we show a basic plot for
 # %%
 
 # Note we set depth=True to define the depth as 2800
-fig, ax = m.plot_layer(field = 't', radius = 2800, depth=True, show=False)
+fig, ax = m.plot_layer(field="t", radius=2800, depth=True, show=False)
 fig.set_size_inches(8, 6)
-ax.set_title('Temperature field at 2800 km depth')
+ax.set_title("Temperature field at 2800 km depth")
 plt.show()
 
 # %% [markdown]
 """
-We can do the same thing with other scalar fields such as the bulk composition. 
+We can do the same thing with other scalar fields such as the bulk composition.
 """
 
 # %%
@@ -52,42 +56,42 @@ We can do the same thing with other scalar fields such as the bulk composition.
 # Add a bulk composition field to the model.
 m.calc_bulk_composition()
 
-# Note bulk composition is in the "c" field. 
-fig, ax = m.plot_layer(field = 'c', radius = 2800, depth=True, show=False)
+# Note bulk composition is in the "c" field.
+fig, ax = m.plot_layer(field="c", radius=2800, depth=True, show=False)
 fig.set_size_inches(8, 6)
-ax.set_title('Bulk composition at 2800 km depth')
+ax.set_title("Bulk composition at 2800 km depth")
 plt.show()
 
 
 # %% [markdown]
 """
-Rather than defining a radius, we can give an index for the layer we want to plot. 
+Rather than defining a radius, we can give an index for the layer we want to plot.
 """
 
 # %%
 
-fig, ax = m.plot_layer(field = 't', index=0, show=False)
+fig, ax = m.plot_layer(field="t", index=0, show=False)
 fig.set_size_inches(8, 6)
-ax.set_title('Temperature at the highest radius.')
+ax.set_title("Temperature at the highest radius.")
 plt.show()
 
 # %% [markdown]
 """
-We can also change the sampling resolution by varying the delta argument. 
+We can also change the sampling resolution by varying the delta argument.
 """
 
 # %%
 
-# plot with intervals of 5 degrees on longitude and latitude. 
-fig, ax = m.plot_layer(field = 't', radius=2800, depth=True, delta=5, show=False)
+# plot with intervals of 5 degrees on longitude and latitude.
+fig, ax = m.plot_layer(field="t", radius=2800, depth=True, delta=5, show=False)
 fig.set_size_inches(8, 6)
-ax.set_title('Temperature on a 5$^{\circ}$ grid at 2800 km depth.')
+ax.set_title("Temperature on a 5$^{\circ}$ grid at 2800 km depth.")
 plt.show()
 
 
 # %% [markdown]
 """
-Rather than a global plot we can specify a local region to plot. 
+Rather than a global plot we can specify a local region to plot.
 """
 
 # %%
@@ -100,8 +104,8 @@ max_la = 30
 
 region = (min_lo, max_lo, min_la, max_la)
 
-fig, ax = m.plot_layer(field = 't', radius=2800, depth=True, show=False, extent=region)
+fig, ax = m.plot_layer(field="t", radius=2800, depth=True, show=False, extent=region)
 fig.set_size_inches(8, 6)
-ax.set_title('Temperature in a small region at 2800 km depth.')
+ax.set_title("Temperature in a small region at 2800 km depth.")
 ax.set_extent(region, ccrs.PlateCarree())
 plt.show()
