a79da9681413065aaaec517a95182e98cf56d232
jamespanton93 <pantonjc@cardiff.ac.uk>
2024-03-08 12:14:37 +0000

    Update __init__.py
    Add zenodo DOI 
    
    Signed-off-by: jamespanton93 <pantonjc@cardiff.ac.uk>

--- terratools/__init__.py
+++ terratools/__init__.py
@@ -1,6 +1,7 @@
 """
 Tools to read, analyse and visualise models written by the TERRA mantle convection code.
 TerraTools is released under an [MIT License](https://opensource.org/licenses/MIT).
+This version's DOI: 10.5281/zenodo.10797186
 
 &copy; The MC² team (James Panton, Andy Nowacki and others)
 """
